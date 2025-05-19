7009debaeb2c655c44eea7c7c6486f8019d4e08e
xueyixuepy <3133719015@qq.com>
2025-05-19 18:49:55 +0800

    手动补充

--- aboutFile.cpp
+++ aboutFile.cpp
@@ -1,4 +1,28 @@
 #define  _CRT_SECURE_NO_WARNINGS
-#include <iostream>
 #include "aboutFile.h"
-using namespace std;
+
+
+
+int getHighestScore()
+{
+	ifstream inFile("score.txt");
+    string score;
+    if (inFile.is_open()) {
+        getline(inFile, score);
+        inFile.close();
+        return stoi(score);
+    }
+    else {
+        cout << "无法打开文件";
+        return -1;
+    }
+}
+void writeHighestScore(int score)
+{
+    ofstream outFile("score.txt");  // 创建并打开文件
+
+    if (outFile.is_open()) {
+        outFile << score;
+        outFile.close();  // 关闭文件
+    }
+}
--- aboutFile.h
+++ aboutFile.h
@@ -1,1 +1,8 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int getHighestScore();
+void writeHighestScore(int score);
